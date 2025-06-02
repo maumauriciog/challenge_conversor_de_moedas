d48b2d8bbfaaecda26a1f4ce26b477f38b3faed0
Task View <maumauriciog@hotmail.com>
2025-06-02 14:50:20 -0300

    corrigindo as excessões

--- src/br/com/challenge/conversordemoedas/principal/Main.java
+++ src/br/com/challenge/conversordemoedas/principal/Main.java
@@ -1,5 +1,9 @@
+package br.com.challenge.conversordemoedas.principal;
+
 import br.com.challenge.conversordemoedas.modelos.ConvertCurrency;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -20,22 +24,22 @@ public class Main {
             System.out.println("5). Dolar           -> Peso Colombiano");
             System.out.println("6). Peso Colombiano -> Dolar");
             System.out.println("7). Sair\n");
-            System.out.print("-> Escolha uma opção válida: ");
-            option = imput.nextInt();
-
-            if (option == 7) {
-                System.out.println("**** Conversor de Moedas Finalizado ****");
-                break;
-            }
-
-            System.out.print("-> Digite o valor que deseja converter: ");
-            double amount = imput.nextInt();
-
-            if (option <= 0 || option > 7) {
-                System.out.println("\n*** Opção Inválida! Repita a Operação ***\n");
-            }
 
             try {
+                System.out.print("-> Escolha uma opção válida: ");
+                option = imput.nextInt();
+
+                if (option >= 7) {
+                    System.out.println("**** Programa de Conversor de Moedas Finalizado ****");
+                    break;
+                }
+
+                System.out.print("-> Digite o valor que deseja converter: ");
+                double amount = imput.nextInt();
+
+                if ((option <= 0) | (option > 7)) {
+                    System.out.println("\n*** Opção Inválida! Repita a Operação ***\n");
+                }
                 switch (option) {
                     case 1:
                         convert.Currency("USD", "ARS", amount);
@@ -56,9 +60,11 @@ public class Main {
                         convert.Currency("COP", "USD", amount);
                         break;
                 }
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+            } catch (InputMismatchException i) {
+                System.out.println("\n*** Você digitou valores INCORRETOS, por favor execute novamente o programa...***");
+                break;
+            } catch (InterruptedException | IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
